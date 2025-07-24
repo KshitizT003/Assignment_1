2ae7453f4a696f7a9292b2e1ad8c565a1bb49d8e
KshitizT003 <tamangkshitiz90@gmail.com>
2025-07-24 09:40:38 -0400

    Made the layout for the home page

--- app/src/main/java/com/example/assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/assignment_1/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.assignment_1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assignment_1.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        binding.searchButton.setOnClickListener {
+            val query = binding.searchInput.text.toString().trim()
+
+            }
         }
     }
-}
+
